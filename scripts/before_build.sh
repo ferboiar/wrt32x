fa72cfd918995b67a8c27c8f4b7db0940aac609e
Fer <30288888+ferboiar@users.noreply.github.com>
2021-08-13 14:36:08 +0200

    Update before_build.sh

--- scripts/before_build.sh
+++ scripts/before_build.sh
@@ -3,10 +3,7 @@
 # Enter your commands here, e.g.
 # echo "Start build!"
 cd openwrt
-
-#reset this commit: https://github.com/openwrt/openwrt/commit/29a3967e61334d0c6a1a7d391f0e751272d77b1d
-# generic: fix kernel panic on existing mac-address node
-# probar, si compila dejarlo, si no quitarlo
-git reset --hard 29a3967e61334d0c6a1a7d391f0e751272d77b1d
+# git reset --hard 29a3967e61334d0c6a1a7d391f0e751272d77b1d
+git revert --no-edit eaa9c94c75748265b9260691d5f59c9d3711f7d4
 
 exit 0

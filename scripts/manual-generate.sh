e7ed328a9410b6aa53531cce8cb405ece3d461c8
Fer <30288888+ferboiar@users.noreply.github.com>
2021-12-15 20:10:49 +0100

    Update manual-generate.sh

--- scripts/manual-generate.sh
+++ scripts/manual-generate.sh
@@ -2,7 +2,7 @@
 #
 #   Copyright (C) 2006-2021
 #
-# Updated: 12/20/2020
+# Updated: 11/28/2021
 #
 #   This program is free software; you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
@@ -32,16 +32,101 @@
 # Make your special changes then save as device needed:
 #
 ### ============================================= ###
+echo "Make sure you have:"
+echo "-------------------"
+echo "                   "
+echo "scripts (Directory)"
+echo "       diy-part1.sh"
+echo "       diy-part2.sh"
+echo "       luci_themes.sh"
+echo "       DevOpenWRT-Router.sh"
+echo "       lean_packages.sh"
+echo "       sirpdboy-package.sh"
+echo "configs (Directory)"
+echo "       feeds.conf.default"
+echo "       wrt3200acm.config"
+echo "       patches (Directory)"
+echo "                          "
+sleep 5
+### ------------------------------------------------------------------------------- ###
+FILE=diy-part1.sh
+if [ ! -f "$FILE" ]; then
+    echo "$FILE does not exist."
+    exit
+fi
+FILE=diy-part2.sh
+if [ ! -f "$FILE" ]; then
+    echo "$FILE does not exist."
+    exit
+fi
+FILE=luci_themes.sh
+if [ ! -f "$FILE" ]; then
+    echo "$FILE does not exist."
+    exit
+fi
+FILE=DevOpenWRT-Router.sh
+if [ ! -f "$FILE" ]; then
+    echo "$FILE does not exist."
+    exit
+fi
+FILE=lean_packages.sh
+if [ ! -f "$FILE" ]; then
+    echo "$FILE does not exist."
+    exit
+fi
+FILE=sirpdboy-package.sh
+if [ ! -f "$FILE" ]; then
+    echo "$FILE does not exist."
+    exit
+fi
+FILE=feeds.conf.default
+if [ ! -f "$FILE" ]; then
+    echo "$FILE does not exist."
+    exit
+fi
+FILE=wrt3200acm.config
+if [ ! -f "$FILE" ]; then
+    echo "$FILE does not exist. Or Dif device Chosen, Change Name here:"
+    exit
+fi
+FILE=patches
+if [ ! -d "$FILE" ]; then
+    echo "$FILE Directory not exist."
+    exit
+fi
+### ------------------------------------------------------------------------------- ###
+
+echo "Running: diy-part1.sh"
 ./diy-part1.sh
+
+echo "Cloning from: luci_themes.sh"
 ./luci_themes.sh
+
+echo "Cloning from: DevOpenWRT-Router.sh"
 ./DevOpenWRT-Router.sh
+
+echo "Cloning from: lean_packages.sh"
 ./lean_packages.sh
+
+echo "Cloning from: sirpdboy-package.sh"
 ./sirpdboy-package.sh
+
+echo "Running: update -a, install -a, uninstall bluld"
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 ./scripts/feeds uninstall bluld
+
+echo "copy wrt3200acm.config .config"
 cp wrt3200acm.config .config
+
+echo "Applying Patches"
 git am patches/*.patch
+
+echo "Running: diy-part2.sh"
 ./diy-part2.sh
+
+echo "Make Menuconfig"
 make menuconfig
+
+echo"Finished: EXIT"
 exit 0

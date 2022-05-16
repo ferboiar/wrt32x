6161a181f844af62b87abb339be94ed6bca7f178
Fer <30288888+ferboiar@users.noreply.github.com>
2022-05-16 12:46:16 +0200

    Update fetch_packages.sh

--- scripts/fetch_packages.sh
+++ scripts/fetch_packages.sh
@@ -164,6 +164,7 @@ rm -rf package/kenzok8/luci-app-bypass
 rm -rf package/kenzok8/luci-app-passwall
 rm -rf package/kenzok8/luci-app-passwall2
 rm -rf package/kenzok8/luci-app-ssr-plus
+rm -rf package/kenzok8/luci-app-baidupcs-web
 rm -rf package/kenzok8/baidupcs-web
 rm -rf package/kenzok8/luci-theme-argon # borro este y me quedo con el original
 rm -rf package/kenzok8/luci-app-argon-config # borro este y me quedo con el original

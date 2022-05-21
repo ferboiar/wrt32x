051a29de248b6ee637d841d0d187b3de2b1ef218
Fer <30288888+ferboiar@users.noreply.github.com>
2022-05-21 21:52:18 +0200

    Update after_build.sh

--- scripts/after_build.sh
+++ scripts/after_build.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #cd openwrt || exit
-cd openwrt
+#cd openwrt
 
-if [[ -f "usr/lib/acme/acme.sh" ]]; then
-  if [[ ! -f "usr/lib/acme/acme.sh.bak" ]]; then
-    echo "Backing up original acme.sh script..."
-    mv usr/lib/acme/acme.sh usr/lib/acme/acme.sh.bak
-  fi
-  echo "Downloading acme.sh script from repo..."
-  wget -q -O usr/lib/acme/acme.sh https://raw.githubusercontent.com/acmesh-official/acme.sh/master/acme.sh
-  echo "Download complete, you may now enjoy a up to date acme.sh script"
-  else echo "acme.sh file not found, perhaps acme isn't installed"
-fi
+#if [[ -f "usr/lib/acme/acme.sh" ]]; then
+#  if [[ ! -f "usr/lib/acme/acme.sh.bak" ]]; then
+#    echo "Backing up original acme.sh script..."
+#    mv usr/lib/acme/acme.sh usr/lib/acme/acme.sh.bak
+#  fi
+#  echo "Downloading acme.sh script from repo..."
+#  wget -q -O usr/lib/acme/acme.sh https://raw.githubusercontent.com/acmesh-official/acme.sh/master/acme.sh
+#  echo "Download complete, you may now enjoy a up to date acme.sh script"
+#  else echo "acme.sh file not found, perhaps acme isn't installed"
+#fi
 
 # Enter your commands here, e.g.
 # echo "Success!"
